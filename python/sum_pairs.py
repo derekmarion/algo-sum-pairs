6d4bfcbb4117f688398392414c1da954b3d4a5c9
Derek Marion <derek.j.marion@gmail.com>
2023-10-08 10:01:55 -0700

    First working version of sum_pairs.py that satisfies all test cases

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,13 @@
-def sum_pairs():
+def sum_pairs(arr, num):
+    output = []
+    to_skip = ()
+    for element in arr:
+        current_num = element
+        to_skip += (current_num,)
+        for sub_element in arr:
+            if sub_element + current_num == num and sub_element not in to_skip:
+                sub_arr = [current_num, sub_element]
+                output.append(sub_arr)
+    if output == []:
+        return "unable to find pairs"
+    return output
