4ed6abed6e247b1418261dd4725795c306fc4149
Derek Marion <derek.j.marion@gmail.com>
2023-10-08 09:43:53 -0700

    Add initial tests for sum_pairs_spec.py

--- python/sum_pairs_spec.py
+++ python/sum_pairs_spec.py
@@ -1,3 +1,6 @@
 from sum_pairs import sum_pairs
 
-# write your specs here!
+def test():
+    assert sum_pairs([1,2,3,4,5], 9) == [[4,5]]
+    assert sum_pairs([1,2,3,4,5], 7) == [[2,5], [3,4]]
+    assert sum_pairs([3, 1, 5, 8, 2], 27) == 'unable to find pairs'
