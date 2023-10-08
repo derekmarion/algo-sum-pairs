e37cf1dcef90de7ab2a33cc639c1176f5b4bb373
Derek Marion <derek.j.marion@gmail.com>
2023-10-08 12:00:52 -0700

    JS - working sumPairs function that satisfies all test cases

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,19 @@
-exports.sumPairs = function() {
-
+sumPairs = function(arr, num) {
+    let output = [];
+    let toSkip = [];
+    for (element of arr) {
+        let currentNum = element;
+        toSkip.push(currentNum);
+        for (subElement of arr) {
+            if ((currentNum + subElement == num) && (!(toSkip.includes(subElement)))) {
+                output.push([currentNum, subElement]);
+            };
+        };
+    };
+    if (output.length != 0) {
+    return output;
+    };
+    return "unable to find pairs"
 };
+
+module.exports = {sumPairs};
